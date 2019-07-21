38b80de99366b8eaba743f4c9d9b9b8871d263c5
gpi <gpimpale29@gmail.com>
2019-07-20 19:37:59 -0700

    X11.c works

--- c/x11.c
+++ c/x11.c
@@ -1,103 +1,79 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+
 #include <X11/Xlib.h>
-#include <X11/Xutil.h>
 #include <X11/Xos.h>
+#include <X11/Xutil.h>
 
-typedef struct {
-  Display *dis;
-  Window win;
-  GC gc;
-  uint32_t xSize;
-  uint32_t ySize;
-} Canvas;
+#define FRAME_XSIZE 500
+#define FRAME_YSIZE 500
 
-void initX(Canvas *canvas, uint32_t xSize, uint32_t ySize) {
-  Display *dis = XOpenDisplay((char *)0);
-  Window win = XCreateSimpleWindow(dis, DefaultRootWindow(dis), 0, 0, xSize,
-                                   ySize, 0, 0, 0);
-  GC gc = XCreateGC(dis, win, 0, 0);
-  XSetStandardProperties(dis,win,"My Window","HI!",None,NULL,0,NULL);
-  XSetBackground(dis, gc, 0);
-  XSetForeground(dis, gc, 0);
-  XClearWindow(dis, win);
-  XMapRaised(dis, win);
-
-  canvas->dis = dis;
-  canvas->win = win;
-  canvas->gc = gc;
-  canvas->xSize = xSize;
-  canvas->ySize = ySize;
-}
-
-void freeX(Canvas *canvas) {
-  XFreeGC(canvas->dis, canvas->gc);
-  XDestroyWindow(canvas->dis, canvas->win);
-  XCloseDisplay(canvas->dis);
-}
+/* here are our X variables */
+Display *dis;
+int screen;
+Window win;
+GC gc;
 
 void init_x() {
-  Display *dis;
-  int screen;
-  Window win;
-  GC gc;
-	/* get the colors black and white (see section for details) */
-	unsigned long black,white;
+  /* get the colors black and white (see section for details) */
+  unsigned long black, white;
 
-	/* use the information from the environment variable DISPLAY 
-	   to create the X connection:
-	*/	
-	dis=XOpenDisplay((char *)0);
-   	screen=DefaultScreen(dis);
-	black=BlackPixel(dis,screen),	/* get color black */
-	white=WhitePixel(dis, screen);  /* get color white */
+  dis = XOpenDisplay((char *)0);
+  screen = DefaultScreen(dis);
+  black = BlackPixel(dis, screen), white = WhitePixel(dis, screen);
+  win = XCreateSimpleWindow(dis, DefaultRootWindow(dis), 0, 0, FRAME_XSIZE,
+                            FRAME_YSIZE, 5, black, white);
+  XSetStandardProperties(dis, win, "Howdy", "Hi", None, NULL, 0, NULL);
+  XSelectInput(dis, win, ExposureMask | ButtonPressMask | KeyPressMask);
+  gc = XCreateGC(dis, win, 0, 0);
+  XSetBackground(dis, gc, white);
+  XSetForeground(dis, gc, black);
+  XClearWindow(dis, win);
+  XMapRaised(dis, win);
+  //
+}
 
-	/* once the display is initialized, create the window.
-	   This window will be have be 200 pixels across and 300 down.
-	   It will have the foreground white and background black
-	*/
-   	win=XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0,	
-		200, 300, 5, white, black);
+void close_x() {
+  XFreeGC(dis, gc);
+  XDestroyWindow(dis, win);
+  XCloseDisplay(dis);
+}
 
-	/* here is where some properties of the window can be set.
-	   The third and fourth items indicate the name which appears
-	   at the top of the window and the name of the minimized window
-	   respectively.
-	*/
-	XSetStandardProperties(dis,win,"My Window","HI!",None,NULL,0,NULL);
+XEvent event;    // the XEvent declaration !!!
+KeySym key;      // a dealie-bob to handle KeyPress Events
+char text[255];  // a char buffer for KeyPress Events
 
-	/* this routine determines which types of input are allowed in
-	   the input.  see the appropriate section for details...
-	*/
-	XSelectInput(dis, win, ExposureMask|ButtonPressMask|KeyPressMask);
+void handle_user_input() {
+  // get the next event and stuff it into our event variable.
+  // Note:  only events we set the mask for are detected!
 
-	/* create the Graphics Context */
-        gc=XCreateGC(dis, win, 0,0);        
+  XNextEvent(dis, &event);
+  if (event.type == Expose && event.xexpose.count == 0) {
+    // the window was exposed redraw it!
+    // redraw();
+  }
+  if (event.type == KeyPress &&
+      XLookupString(&event.xkey, text, 255, &key, 0) == 1) {
+    // use the XLookupString routine to convert the invent
+    //   KeyPress data into regular text.  Weird but necessary...
 
-	/* here is another routine to set the foreground and background
-	   colors _currently_ in use in the window.
-	*/
-	XSetBackground(dis,gc,white);
-	XSetForeground(dis,gc,black);
-
-	/* clear the window and bring it on top of the other windows */
-	XClearWindow(dis, win);
-	XMapRaised(dis, win);
-};
+    if (text[0] == 'q') {
+      close_x();
+    }
+    printf("You pressed the %c key!\n", text[0]);
+  }
+  if (event.type == ButtonPress) {
+    // tell where the mouse Button was Pressed
+    //	int x=event.xbutton.x,
+    //	    y=event.xbutton.y;
+  }
+}
 
 int main() {
-
   init_x();
-
-  for(;;);
-
-  XInitThreads();
-
-  Canvas c;
-  initX(&c, 500, 500);
-
-  XDrawLine(c.dis, c.win, c.gc, 50, 50, 100, 100);
-
-  freeX(&c);
+  while(true) {
+    handle_user_input();
+  }
   return 0;
 }
