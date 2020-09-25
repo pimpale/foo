81e5d553ba5bebfe9d4adf69118458321b79fd62
gpi <gpimpale29@gmail.com>
2020-09-24 17:32:50 -0700

    updated longestpalindromicsequence to not take so long

--- python/longestpalindromicsequence.py
+++ python/longestpalindromicsequence.py
@@ -1,21 +1,67 @@
-def palindrome(s:str) -> bool:
-	slen = len(s)
-	for i in range(slen//2):
-		if s[i] != s[slen-1-i]:
-			return False
-	return True
+def isOddShellPal(s:str, slen:int, i:int, r:int) -> bool:
+    li = i-r
+    hi = i+r
+
+    if li < 0 or hi >= slen:
+        return False
+    return s[li] == s[hi]
+
+# ibefore is the index of the element before the point we want to check
+def isEvenShellPal(s:str, slen:int, ibefore:int, r:int) -> bool:
+    li = ibefore-r+1
+    hi = ibefore+r
+
+    if li < 0 or hi >= slen:
+        return False
+    return s[li] == s[hi]
+
+def getOddPal(s:str, i:int, r:int) -> str:
+    li = i-r
+    hi = i+r
+    return s[li:hi+1]
+
+
+# ibefore is the index of the element before the point we want to check
+def getEvenPal(s:str, ibefore:int, r:int) -> str:
+    li = ibefore-r+1
+    hi = ibefore+r
+    return s[li:hi+1]
 
 def longestPalindrome(s: str) -> str:
-	lpalstr = ""
-	slen = len(s)
-	for i in range(slen):
-		for j in range(i+1, slen+1):
-			subs = s[i:j]
-			if palindrome(subs):
-				if len(lpalstr) < len(subs):
-					lpalstr = subs
-	return lpalstr
+    bigpal = ""
+    bigpallen = 0
+
+    slen = len(s)
+
+    # odd check
+    for i in range(slen):
+        r = 0
+
+        while True:
+            if not isOddShellPal(s,slen, i, r+1):
+                break
+            r += 1
+
+        pallen = r*2 +1
+        if pallen > bigpallen:
+            bigpal = getOddPal(s, i, r)
+            bigpallen = pallen
+
+    # even check
+    for i in range(slen):
+        r = 0
+
+        while True:
+            if not isEvenShellPal(s,slen, i, r+1):
+                break
+            r += 1
+
+        pallen = r*2
+        if pallen > bigpallen:
+            bigpal = getEvenPal(s, i, r)
+            bigpallen = pallen
+
+    return bigpal
 
 
-
-print(longestPalindrome("aba 2aba"))
+print(longestPalindrome("abc baab"))
