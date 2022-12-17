29448daec1e45ea61311b2139844179e2ebaee60
gpi <gpimpale29@gmail.com>
2022-12-17 12:20:05 -0800

    stuff

--- python/rl/testtraining.py
+++ python/rl/testtraining.py
@@ -4,7 +4,9 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import backend as K
 
-INP_SIZE = 4
+# tf.debugging.set_log_device_placement(True)
+
+INP_SIZE = 10000
 
 # Goal: Train neural network to add numbers
 
@@ -13,7 +15,11 @@ inp_a = keras.layers.Input(shape=(INP_SIZE,), dtype="float32")
 inp_b = keras.layers.Input(shape=(INP_SIZE,), dtype="float32")
 concat = keras.layers.Concatenate()([inp_a, inp_b])
 
-output = keras.layers.Dense(1, activation='linear')(concat);
+hidden0 = keras.layers.Dense(1000, activation='relu')(concat);
+hidden1 = keras.layers.Dense(1000, activation='relu')(hidden0);
+hidden2 = keras.layers.Dense(1000, activation='relu')(hidden1);
+
+output = keras.layers.Dense(1, activation='linear')(hidden2);
 
 
 model = keras.Model( inputs=[ inp_a, inp_b ], outputs=[ output ] )
@@ -37,29 +43,31 @@ from time import sleep
 
 BATCH_SIZE = 5000
 
-while True:
-    # Come up with some training data
-    # 5 length 4 vectors
-    v_a = np.random.sample((BATCH_SIZE,INP_SIZE))
-    v_b = np.random.sample((BATCH_SIZE,INP_SIZE))
 
-    print("SOURCE DATA:");
-    print(v_a, "\n", v_b)
+with tf.device('/GPU:0'):
+    while True:
+        # Come up with some training data
+        # 5 length 4 vectors
+        v_a = np.random.sample((BATCH_SIZE,INP_SIZE))
+        v_b = np.random.sample((BATCH_SIZE,INP_SIZE))
 
-    # Show what model currently predicts
-    ret = model.predict(
-      [v_a, v_b]
-    )
-    print("OUTPUT:\n", ret)
+        print("SOURCE DATA:");
+        print(v_a, "\n", v_b)
 
-    true_batch = np.zeros((BATCH_SIZE, 1))
-    for i in range(0, BATCH_SIZE):
-        true_batch[i][0] = np.sum(v_a[i])  + np.sum(v_b[i])
+        # Show what model currently predicts
+        ret = model.predict(
+          [v_a, v_b]
+        )
+        print("OUTPUT:\n", ret)
+
+        true_batch = np.zeros((BATCH_SIZE, 1))
+        for i in range(0, BATCH_SIZE):
+            true_batch[i][0] = np.sum(v_a[i])  + np.sum(v_b[i])
 
 
-    print("TRUE COMPLETION DATA:\n", true_batch)
+        print("TRUE COMPLETION DATA:\n", true_batch)
 
-    model.fit(
-      [v_a, v_b],
-      true_batch
-    )
+        model.fit(
+          [v_a, v_b],
+          true_batch
+        )
