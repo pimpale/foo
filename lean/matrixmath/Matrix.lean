c253a91951f8df7c2058d20a04531602ff4a1d38
gpi <gpimpale29@gmail.com>
2023-02-08 02:09:02 -0800

    finished the matrix multiplication

--- lean/matrixmath/Matrix.lean
+++ lean/matrixmath/Matrix.lean
@@ -5,14 +5,64 @@ structure Matrix (α :Type u) (m: Nat) (n: Nat) where
   rows: Vector (Vector α n) m
 deriving Repr
 
-def Matrix.replicate {α : Type u} (m: ℕ) (n: ℕ) (a: α) : Matrix α m n :=
-  { rows  := Vector.replicate m (Vector.replicate n a) }
+namespace Matrix
+def replicate (m: ℕ) (n: ℕ) (a: α) : Matrix α m n := {
+  rows := Vector.replicate m (Vector.replicate n a)
+}
 
-def Matrix.zeros (α : Type u) [Zero α] (m: Nat) (n:Nat) : Matrix α m n :=
-  Matrix.replicate m n 0
- 
-def Matrix.row {α: Type u} (v : Vector α n) : Matrix α 1 n := 
+def ofFn (m: ℕ) (n: ℕ) (f: Fin m → Fin n → α) : Matrix α m n := {
+  rows := Vector.ofFn m (fun i => Vector.ofFn n (f i))
+}
+
+ /-- Create a row matrix from a vector -/
+def row (v : Vector α n) : Matrix α 1 n := 
   { rows := Vector.singleton v } 
 
-def Matrix.col {α : Type u} (v: Vector α m) : Matrix α m 1 :=
+/-- Create a column matrix from a vector -/
+def col (v: Vector α m) : Matrix α m 1 :=
   { rows := Vector.map Vector.singleton v }
+
+/-- Get a row of the matrix as a vector -/
+def getRow (x : Matrix α m n) (i : Fin m): Vector α n :=
+  x.rows.get i
+
+/-- Get a column of the matrix as a vector -/
+def getCol (x: Matrix α m n) (i : Fin n): Vector α m := 
+  x.rows.map (fun v => v.get i)
+
+/-- Update a row in the matrix -/
+def setRow (x : Matrix α m n) (i : Fin m) (v: Vector α n): Matrix α m n :=
+  { rows := x.rows.set i v }
+
+/-- Update a column in the matrix -/
+def setCol (x : Matrix α m n) (i : Fin n) (v: Vector α m): Matrix α m n :=
+  { rows := x.rows.zipWith (fun v a => v.set i a) v }
+
+/-- Get an element in the matrix -/
+def getElem (x : Matrix α m n) (row : Fin m) (col: Fin n): α :=
+  (x.getRow row).get col
+
+/-- Update an element in the matrix -/
+def setElem (x : Matrix α m n) (row : Fin m) (col: Fin n) (a:α) : Matrix α m n :=
+  x.setRow row ((x.getRow row).set col a)
+
+/-- Transpose a matrix -/
+def transpose (x : Matrix α m n) : Matrix α n m :=
+  Matrix.ofFn n m (fun i j =>  x.getElem j i)
+
+@[inherit_doc]
+scoped postfix:1024 "ᵀ" => Matrix.transpose
+
+def zeros (α : Type u) [Zero α] (m: Nat) (n:Nat) : Matrix α m n :=
+  Matrix.replicate m n 0
+
+def identity (α : Type u) (n :ℕ) [Zero α] [One α] : Matrix α n n :=
+  Matrix.ofFn n n (fun i j => if i == j then 1 else 0)
+
+def mul {α : Type u} [Zero α] [Add α] [Mul α] {m₁ : ℕ} {p : ℕ} {n₂ : Nat} (a : Matrix α m₁ p) (b : Matrix α p n₂) : Matrix α m₁ n₂ :=
+  let rows := a.rows;
+  let cols := (bᵀ).rows;
+  Matrix.ofFn m₁ n₂ (fun i j => Vector.dot (rows.get i) (cols.get j))
+
+
+end Matrix
--- lean/matrixmath/Vector.lean
+++ lean/matrixmath/Vector.lean
@@ -3,51 +3,51 @@ import Mathlib.Algebra.Ring.Defs
 import Mathlib.Init.Algebra.Functions
 import Mathlib.Order.Basic
 
-
 structure Vector (α : Type u) (n: ℕ) where
   data: List α
   -- a proof that the data.length = n
   isEq: data.length = n
 deriving Repr
 
-theorem length_replicate {α : Type u}  (n:ℕ) (a:α) :
+namespace Vector 
+
+theorem length_replicate (n:ℕ) (a:α) :
   (List.replicate n a).length = n :=
   by
     simp
 
-def Vector.empty (α : Type u) : Vector α 0 := {
+def empty (α : Type u) : Vector α 0 := {
   data := List.nil
   isEq := List.length_nil
 }
 
-def Vector.replicate {α : Type u} (n: Nat) (x: α) : Vector α n := {
+def replicate (n: ℕ) (x: α) : Vector α n := {
     data := List.replicate n x,
     isEq := length_replicate n x
 }
 
-def Vector.singleton {α : Type u} (x:α) : Vector α 1 := 
+def singleton (x:α) : Vector α 1 := 
   Vector.replicate 1 x
 
-def Vector.zeros (α : Type u) [Zero α] (n: Nat) : Vector α n :=
-  Vector.replicate n 0
-
-def Vector.ones (α : Type u) [One α] (n: Nat) : Vector α n :=
-  Vector.replicate n 1
-
-def Vector.get {α : Type u} {n: Nat} (v: Vector α n) (i : Fin n) :=
+def get (v: Vector α n) (i : Fin n) : α :=
   List.get 
     v.data 
     -- we prove that if i < n then i < data.length
     (Fin.mk i.val (lt_of_lt_of_eq i.isLt (Eq.symm v.isEq))) 
 
-def Vector.dot {α : Type u} [Add α] [Mul α] [Zero α] {n: ℕ} (v1: Vector α n) (v2: Vector α n) : α :=
-  List.foldl Add.add 0 (List.zipWith Mul.mul v1.data v2.data )
+def set (v: Vector α n) (i : Fin n) (a : α) : Vector α n :=
+  -- prove that i ≤ v.data.length
+  let i := Fin.mk i.val (lt_of_lt_of_eq i.isLt (Eq.symm v.isEq));
+  {
+    data := List.set v.data i a,
+    isEq := Eq.trans (List.length_set v.data i a) v.isEq 
+  }
 
 theorem min_of_same {α : Type u} [LinearOrder α] {a : α} {b : α} (h_1 : a = b) :
   min a b = a :=
     min_eq_left (le_of_eq h_1)
 
-def Vector.zipWith {α : Type u} {β : Type u} {γ : Type u} {n: Nat} (f: α → β → γ) (v1: Vector α n) (v2: Vector β n): Vector γ n :=
+def zipWith {α : Type u} {β : Type u} {γ : Type u} {n: Nat} (f: α → β → γ) (v1: Vector α n) (v2: Vector β n): Vector γ n :=
   -- prove that v1 and v2 are the same length
   let v1_v2_same_len := Eq.trans v1.isEq (Eq.symm v2.isEq);
   -- prove that min (v1.length) (v2.length) = n
@@ -57,19 +57,46 @@ def Vector.zipWith {α : Type u} {β : Type u} {γ : Type u} {n: Nat} (f: α →
        isEq := Eq.trans (List.length_zipWith f v1.data v2.data) min_of_v1_v2_is_n
   }
 
-def Vector.map {α : Type u} {β : Type u} {n: ℕ} (f: α → β) (v: Vector α n) : Vector β n := {
+def map {α : Type u} {β : Type u} {n: ℕ} (f: α → β) (v: Vector α n) : Vector β n := {
   data := List.map f v.data,
   isEq := Eq.trans (List.length_map v.data f) v.isEq   
 }
 
-def Vector.add {α : Type u} [Add α] {n: ℕ} (v1: Vector α n) (v2: Vector α n) : Vector α n :=
-  Vector.zipWith Add.add v1 v2
+def cons {α : Type u} {n: ℕ} (a: α) (v: Vector α n) : Vector α (n.succ) := 
+{
+  data := List.cons a v.data,
+  isEq := Eq.trans (List.length_cons a v.data) (congrArg Nat.succ v.isEq)   
+}
 
-def Vector.sub {α : Type u} [Sub α] {n: ℕ} (v1: Vector α n) (v2: Vector α n) : Vector α n :=
-  Vector.zipWith Sub.sub v1 v2
+def ofFn (n: Nat) (f: Fin n -> α) : Vector α n := 
+  match n with
+  | 0 => Vector.empty α
+  | Nat.succ i => Vector.cons (f 0) (Vector.ofFn i (fun i => f (Fin.succ i)))
 
-def Vector.hadamard {α : Type u} [Mul α] {n: ℕ} (v1: Vector α n) (v2: Vector α n) : Vector α n :=
-  Vector.zipWith Mul.mul v1 v2
 
 instance {α : Type u} : Inhabited (Vector α 0) where
   default := Vector.empty α
+
+instance {α : Type u} [Zero α] (n : ℕ) : Zero (Vector α n) where
+  zero := Vector.replicate n 0
+
+instance {α : Type u} [One α] (n : ℕ) : One (Vector α n) where
+  one := Vector.replicate n 1
+
+instance {α : Type u} [Neg α] {n: ℕ} : Neg (Vector α n) where
+  neg (v: Vector α n):= Vector.map Neg.neg v
+
+instance {α : Type u} [Add α] {n: ℕ} : Add (Vector α n) where
+  add (v1: Vector α n)  (v2: Vector α n):= Vector.zipWith Add.add v1 v2
+
+instance {α : Type u} [Sub α] {n: ℕ} : Sub (Vector α n) where
+  sub (v1: Vector α n)  (v2: Vector α n):= Vector.zipWith Sub.sub v1 v2
+
+def hadamard {α : Type u} [Mul α] {n: ℕ} (v1: Vector α n) (v2: Vector α n) : Vector α n :=
+  Vector.zipWith Mul.mul v1 v2  
+
+def dot {α : Type u} [Add α] [Mul α] [Zero α] {n: ℕ} (v1: Vector α n) (v2: Vector α n) : α :=
+  List.foldl Add.add 0 (List.zipWith Mul.mul v1.data v2.data )
+
+end Vector
+
