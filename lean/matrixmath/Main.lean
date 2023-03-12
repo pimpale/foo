ceb428539d2129405bb02aba9d8e93a27d917796
gpi <gpimpale29@gmail.com>
2023-03-11 23:55:39 -0800

    upgrade lean

--- lean/matrixmath/Main.lean
+++ lean/matrixmath/Main.lean
@@ -13,7 +13,7 @@ def NonTRversion  (v: Vector UInt8 i) (m: Matrix UInt8 i i): IO (UInt64) := do
 def main : IO Unit := do
   let q ← IO.getRandomBytes n.toUSize
   let v2 := ⟨q.data, rfl⟩
-  let m := Matrix.identity UInt8 q.data.size
+  let m := Matrix.one
   let x ← timeit "Non Tail Recursive" (NonTRversion v2 m)
 
   IO.println s!"Done"
