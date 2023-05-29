ec795dd1d93453c62b5e00fe3980f80ae2ffe9b6
gpi <gpimpale29@gmail.com>
2023-05-28 23:07:09 -0700

    stuff

--- python/metadrive_test/metadrive_demo.py
+++ python/metadrive_test/metadrive_demo.py
@@ -1,17 +1,11 @@
 import metadrive  # Import this package to register the environment!
 import gymnasium as gym
 
-def print_keys():
-    for key in gym.registry:
-        print(key)
-        print(gym.registry[key])
-
-env = gym.make("MetaDrive-validation-v0", config=dict(render_mode="human"))
+env = gym.make("MetaDrive-validation-v0", config={"render_mode": "human"})
+#env = metadrive.MetaDriveEnv(config={"render_mode": "human", "num_scenarios": 100})
 env.reset()
 for i in range(1000):
     obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-    print(obs)
-    env.render()
     if terminated or truncated:
         env.reset()
 env.close()
