e9f559f43bafda081c4c55d96358f5bde467c698
gpi <gpimpale29@gmail.com>
2023-08-26 10:36:07 -0700

    update lean

--- python/metadrive_test/metadrive_demo.py
+++ python/metadrive_test/metadrive_demo.py
@@ -1,11 +1,15 @@
 import metadrive  # Import this package to register the environment!
 import gymnasium as gym
 
-env = gym.make("MetaDrive-validation-v0", config={"render_mode": "human"})
-#env = metadrive.MetaDriveEnv(config={"render_mode": "human", "num_scenarios": 100})
+env = gym.make("MetaDrive-validation-v0", config={"use_render": True})
 env.reset()
+
+for _ in range(10):
+    env.step([0,0])
+
+env.vehicle.set_velocity([0, 0], in_local_frame=True)
 for i in range(1000):
-    obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+    obs, reward, terminated, truncated, info = env.step([0, 1])
     if terminated or truncated:
         env.reset()
 env.close()
