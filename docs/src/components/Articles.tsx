7bb7b9b7d60541d2be0de4321d05558e4438fdeb
gpi <gpimpale29@gmail.com>
2025-02-06 01:04:14 -0800

    temp host docs

---
+++ docs/src/components/Articles.tsx
@@ -0,0 +1,30 @@
+import ArticleLayout from '../components/ArticleLayout';
+import Section from '../components/Section';
+
+import { format } from 'date-fns';
+
+import { Article, articleData } from '../components/ArticleData';
+
+type ArticleLinkProps = {
+  a: Article,
+  showDate?: boolean
+}
+
+export const ArticleLink = ({ a, showDate }: ArticleLinkProps) => {
+  if (showDate === true) {
+    return <a href={a.url} children={`${a.name} (${format(a.published, "MMM d, y")})`} />
+  } else {
+    return <a href={a.url} children={a.name} />
+  }
+}
+
+const Articles = () =>
+  <Section id="articles" name="Articles">
+    <ul>
+      {Array.from(articleData.values()).map((a, i) =>
+        <li key={i}><ArticleLink a={a} /></li>
+      )}
+    </ul>
+  </Section>
+
+export default Articles;
