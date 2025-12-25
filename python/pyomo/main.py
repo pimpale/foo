ba063aa9c2b1b13fe766b583057dbfd3f1851cf4
gpi <gpimpale29@gmail.com>
2025-12-25 11:50:50 -0800

    pyomo

---
+++ python/pyomo/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pyomo!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/pyomo/test1.py
@@ -0,0 +1,23 @@
+from pyomo.environ import *
+
+model = ConcreteModel()
+
+# declare decision variables
+model.x = Var(domain=NonNegativeReals)
+
+# declare objective
+model.profit = Objective(
+    expr = 40*model.x,
+    sense = maximize)
+
+# declare constraints
+model.demand = Constraint(expr = model.x <= 40)
+model.laborA = Constraint(expr = model.x <= 80)
+model.laborB = Constraint(expr = 2*model.x <= 100)
+
+# solve
+SolverFactory('cbc').solve(model).write()
+
+print("Solution:")
+model.profit.display()
+model.x.display()
