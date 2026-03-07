0636552e091eb8d211becd5e6e765097abeee6b2
gpi <gpimpale29@gmail.com>
2026-03-06 17:20:40 -0800

    init renderer for abjad experiments

---
+++ python/semitic-cognate-finder/playground/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState, useCallback, useMemo } from 'react';
+
+
+import './style.scss';
+
+// Main App
+export default function App() {
+  
+  const [input, setInput] = useState('');
+
+
+
+  return (
+    <div className="container-fluid py-4">
+
+    </div>
+  );
+}
